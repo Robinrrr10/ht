7cc0393b6ce6fe2c98814ef550eab94d78564a3f
robinrrr10 <robinrrr10@gmail.com>
2020-12-17 23:07:39 +0530

    added ht first commit files

---
+++ cmd/root.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "fmt"
+
+func init() {
+	fmt.Println("currently in root")
+}
+
+func Execute() {
+	fmt.Println("All commands are:")
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Robinrrr10/ht/cmd"
+)
+
+func main() {
+	fmt.Println("Start")
+	cmd.Execute()
+	fmt.Println("end")
+}
